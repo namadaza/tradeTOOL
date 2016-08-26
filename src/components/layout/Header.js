f51ad23aee8ccd071d8ffcf1859509e09f613f4a
namadaza <namadaza@gmail.com>
2016-08-25 23:10:15 -0700

    Updated Header.js with beginings if alt (flux) implementations. Need to flesh out actions/stores. Test if props being passed in and rendering components correctly

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,6 +6,71 @@ import CreateAccount from "./CreateAccount";
 import Login from "./Login";
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = HeaderStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    HeaderStore.listen(this.onChange);
+    HeaderActions.getAccountStatus();
+  }
+
+  componentWillUnmount() {
+    HeaderStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  accountButtons() {
+    if (this.state.userLoggedIn) {
+      return (
+        <div>
+          <li class="buttons">
+            <a href="" class="login" data-toggle="modal" data-target="#myLoginModal">
+              <i class="fa fa-sign-in"></i> Login
+            </a>
+          </li>
+          <li class="buttons">
+            <a href="" class="createacc" data-toggle="modal" data-target="#myCreateAccModal">
+              <i class="fa fa-user"></i> Create Account
+            </a>
+          </li>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <li class="buttons">
+            <a href="" class="login" data-toggle="modal" data-target="#myLoginModal">
+              <i class="fa fa-sign-in"></i> My Account
+            </a>
+          </li>
+        </div>
+      )
+    }
+  }
+
+  showModals() {
+    if (this.state.userLoggedIn) {
+      return (
+        <div>
+          <Login id="myLoginModal" class="modal fade" role="dialog"></Login>
+          <CreateAccount id="myCreateAccModal" class="modal fade" role="dialog"></CreateAccount>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Login id="myLoginModal" class="modal fade" role="dialog"></Login>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div id="wrapper">
@@ -27,21 +92,11 @@ export default class Header extends React.Component {
           </div>
 
           <ul class="nav navbar-right top-nav">
-            <li class="buttons">
-              <a href="" class="login" data-toggle="modal" data-target="#myLoginModal">
-                <i class="fa fa-sign-in"></i> Login
-              </a>
-            </li>
-            <li class="buttons">
-              <a href="" class="createacc" data-toggle="modal" data-target="#myCreateAccModal">
-                <i class="fa fa-user"></i> Create Account
-              </a>
-            </li>
+            {this.accountButtons}
           </ul>
           <Sidenav />
         </nav>
-        <Login id="myLoginModal" class="modal fade" role="dialog"></Login>
-        <CreateAccount id="myCreateAccModal" class="modal fade" role="dialog"></CreateAccount>
+        {this.showModals}
       </div>
     );
   }
